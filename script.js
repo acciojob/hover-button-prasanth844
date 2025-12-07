839f6a27f8d99d74f30641fcd702ee94021f5362
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2025-12-07 16:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+
+Content	Only plain text without any additional tags inside the button
+Hover State
+Property/attribute	Value
+Background Colour	yellow
+Font Colour	black
+Border Radius	30px
